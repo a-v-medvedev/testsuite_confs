5887b81d32d95c7a931367a3253bc6398c70b3b9
Alexey V. Medvedev <alexey.v.medvedev@gmail.com>
2023-03-03 15:34:57 +0300

    Updated to confirm to massivetests changes.

--- teststub/functest/alexey-mars/basic/input_maker_cmdline.sh
+++ teststub/functest/alexey-mars/basic/input_maker_cmdline.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+function fatal() {
+    local msg="$1"
+    echo "FATAL: $msg"
+    exit 1
+}
 
 
 function parse_arg() {
     local arg=$1
-    local varname=$(echo $arg | awk -F= '{print $1}')
-    local value=$(echo $arg | sed "s/$varname=//")
+    local varname=$(echo $arg | awk -F= '{print $1}' | sed 's/MASSIVE_TESTS_TESTITEM_//')
+    local value=$(echo $arg | awk -F= '{print $2}')
     [ -z "$value" -o -z "$varname" ] && exit 1
     eval "$varname=$value"
 }
@@ -14,9 +19,19 @@ for i in $*; do
     parse_arg $i
 done
 
-[ -z "$WLD" ] && exit 1
+[ -z "$WLD" ] && fatal "input_maker_cmdline.sh logic error: workload parameter is empty or not set"
 
 # WLD CONF WPRT WPRT_PARAMS NP PPN TIMEOUT
+#
+# { WLD, CONF } -- worload name and configuration name
+# { WPRT, WPRT_PARAMS } -- workpart name and its numeric parameter (typically, numer of iterations)
+# { NP, PPN } -- number of nodes and number of processes per node for this MPI session
+# TIMEOUT -- numeric timeout parametee in seconds
+#
+# TESTITEM__xxx_yyy  -- records from test_items,yaml that correspond to this test item
+#
+# Return value: non-zero return value means that this test will be skipped, message will be copied to massivetest log
+#
 
 if [ "$MASSIVETEST_AUX_ARGS" == "-mode always -code T"  ]; then
     echo "I'm too lazy for this ($*)" >&2
