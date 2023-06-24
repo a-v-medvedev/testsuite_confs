87f1f81a11b2750d89d50d9b26268b1f5d14cd82
Alexey V. Medvedev <alexey.v.medvedev@gmail.com>
2023-06-24 15:00:24 +0300

    mpi-benchmark: initial config version for functest scripts.

---
+++ mpi-benchmarks/functest/alexey-aero2/basic/input_maker_cmdline.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+function fatal() {
+    local msg="$1"
+    echo "FATAL: $msg"
+    exit 1
+}
+
+
+function parse_arg() {
+    local arg=$1
+    local varname=$(echo $arg | awk -F= '{print $1}' | sed 's/MASSIVE_TESTS_TESTITEM_//')
+    local value=$(echo $arg | awk -F= '{print $2}')
+    [ -z "$value" -o -z "$varname" ] && exit 1
+    eval "$varname=$value"
+}
+
+for i in $*; do
+    parse_arg $i
+done
+
+# This script generates a commant line for a test to run. The command line meant to be the only line
+# which this script outputs to stdout.
+#
+# After parsing the assignments given in the command line, we have the variables:
+#
+# $WLD $CONF $WPRT $WPRT_PARAMS $NP $PPN $TIMEOUT $TESTITEM__xxx_yyy
+#
+# { $WLD, $CONF } -- workload name and configuration name
+# { $WPRT, $WPRT_PARAMS } -- workpart name and its numeric parameter (typically, numer of iterations)
+# { $NP, $PPN } -- number of nodes and number of processes per node for this MPI session
+# $TIMEOUT -- numeric timeout parameter in seconds
+#
+# $TESTITEM__xxx_yyy  -- records from test_items.yaml that correspond to this test item
+#
+# Script return value: non-zero return value means that this test will be skipped, message will be
+# copied to massivetest log
+#
+
+[ -z "$WLD" ] && fatal "input_maker_cmdline.sh logic error: workload parameter is empty or not set"
+[ -z "$WPRT" ] && fatal "input_maker_cmdline.sh logic error: workpart parameter is empty or not set"
+
+if [ "$WRPT" == "sync" ]; then
+    echo "-len $WPRT -ncycles $WPRT_PARAMS -output result.%PSUBMIT_JOBID%.yaml"
+elif [ "$WRPT" == "async" ]; then
+    IMBASYNC_WORKLOAD_PARAMS="cpu_calculations=true:gpu_calculations=false:cycles_per_10usec=$IMBASYNC_CPER10USEC"
+    IMBASYNC_WORKLOAD_PARAMS="$WORKLOAD_PARAMS:manual_progress=true"
+    LEN="4,512,16384,131072,4194304"
+    NCYCLES="10000,1000,50,10,5"
+    [ ! -f calctime_${WLD}_${NP}.txt ] && fatal "No file calctime_${WLD}_${NP}.txt"
+    [ $(wc -l < calctime_${WLD}_${NP}.txt) != "1" ] && fatal "Wrong format of file calctime_${WLD}_${NP}.txt"
+    echo "-datatype char -len $LEN -ncycles $NCYCLES -output result.%PSUBMIT_JOBID%.yaml -workload_params $IMBASYNC_WORKLOAD_PARAMS -calctime $(cat calctime_${WLD}_${NP}.txt) "
+fi
+exit 0
+
