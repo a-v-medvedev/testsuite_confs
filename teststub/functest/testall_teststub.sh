6af65e4ca42bb687553e3d61d9ce6d13b3063b63
Alexey Medvedev <alexey.medvedev@bsc.es>
2025-02-24 14:41:34 +0100

    teststub: generic: config refresh.

--- teststub/functest/testall_teststub.sh
+++ teststub/functest/testall_teststub.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-default_url="https://github.com/a-v-medvedev/testsuite_confs.git"
 default_branch="master"
 default_conf="generic"
 default_suites="basic"
-
+default_hwconf="generic"
 
 app="teststub"
 
+export TESTSUITE_HWCONF=${TESTSUITE_HWCONF:-${default_hwconf}}
 export TESTSUITE_PROJECT="$app"
 export TESTSUITE_BRANCH=${1:-${default_branch}}
-export TESTSUITE_CONF_URL=${default_url}
 export TESTSUITE_SUITES="${2:-${default_suites}}"
 export TESTSUITE_BUILD_CONF=${default_conf}
 
